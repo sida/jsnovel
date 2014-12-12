6455804a874a7511961cafcea0b37c471234a590
mob-shida <shida@mobcast.jp>
2014-12-12 19:26:29 +0900

    いろいろ機能追加

--- tes.js
+++ tes.js
@@ -1,96 +1,13 @@
-var _u = require('underscore');
-var _mut = require('./my_util');
+var _s = require('./lib/scenario');
+var _debug = require('./lib/debug_util');
 
-
-var gSCENARIO = {};
-
-// util
-function isArray(o){
-  return o instanceof Array;
-}
-
-function  isString(o){
-    return (typeof o == 'string');
-}
-
-// API
-function log (mess){
-    _mut.println(mess);
-}
-
-function println (mess){
-    _mut.println(mess);
-}
-
-function print (mess){
-    _mut.print(mess);
-}
-
-function dump(){
-    _u.each(gSCENARIO,function(list,k){
-	println(k + ':{');
-	_u.each(list,function(c){
-	    if (isString(c)){
-		println('  "'+c+'"');
-	    }else{
-		println('  ['+c.name+']');
-	    }
-	});
-	println('}');
-    });
-}
-
-// DSL
-var cr = {name:'cr',exec : function(){_mut.println('');}};
-
-function BLOCK(){
-    if (arguments.length<2){
-        log("Err:not enoght argments");
-	return;
-    }
-    var arg = [];
-    for (var ii=0;ii<arguments.length;ii++){
-	arg[ii] = arguments[ii];
-    }
-    var name = arg.shift();
-    if (name in gSCENARIO){
-        log("Err:exist block "+name);
-	return;
-    }
-    gSCENARIO[name] = arg;
-}
-
-function OVERWRITE_BLOCK(){
-    if (arguments.length<2){
-        log("Err:not enoght argments");
-	return;
-    }
-    var arg = [];
-    for (var ii=0;ii<arguments.length;ii++){
-	arg[ii] = arguments[ii];
-    }
-    var name = arg.shift();
-    delete gSCENARIO[name];
-    if (name in gSCENARIO){
-        log("Err:exist block "+name);
-	return;
-    }
-    gSCENARIO[name] = arg;
-}
-
-function REMOVE_BLOCK(name){
-    delete gSCENARIO[name];
-}
-
+g
 // DSL実行
+var command = 
+"BLOCK ('b1','aaa','bbb',cr,cr)\n" +
+"BLOCK ('b2','ccccc','dd',cr)\n" +
+"OVERWRITE_BLOCK ('name1',cr,'AA','BB',goto('b1'))\n";
+//"BLOCK ('m1','ccccc','dd',keyin(2,'mess1','mess2',goto('b1'),goto('b2')))\n";
 
-BLOCK ('1','aaa','bbb',cr,cr);
-BLOCK ('2','ccccc','dd',cr);
-
-dump();
-
-OVERWRITE_BLOCK ('name1',cr,'AA','BB');
-dump();
-
-
-//_mut.println_dump(gSCENARIO);
+_s.readString(command);
+_debug.dumpScenario();
